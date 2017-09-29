1535b7c9c6a126a892ec30a47966976debe9a33f
leQuimera <lequimera@yandex.ru>
2017-09-29 14:35:20 +0300

    Оформление меню, включаа скрипт

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function() {
+  var navigation = document.querySelector('.page-nav');
+  var toggler = navigation.querySelector('.page-nav__toggle');
+  var menuList = navigation.querySelector('.page-nav__list');
+
+  var onMenuOperation = function (evt) {
+    evt.preventDefault();
+      menuList.classList.toggle('page-nav__list--closed');
+      toggler.classList.toggle('page-nav__toggle--closed');
+  }
+
+  toggler.addEventListener('click', onMenuOperation);
+
+})();
